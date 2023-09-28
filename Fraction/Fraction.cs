b77248a6a84f48f7c2020dc4f9bad952d7bcaad2
Felix Byrjall <felixbyrjall@gmail.com>
2023-09-28 16:36:52 +0200

    added comments in the Fraction.cs file

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -4,24 +4,27 @@ namespace Fraction
 	public class Fraction
 	{
 
-		#region Fields, Constructors
+		#region Fields, constructors and properties
+
+		// A constructor setting default values for the numerator and denominator
 		public Fraction(int numerator = 0, int denominator = 1)
 		{
 			Numerator = numerator;
 			Denominator = denominator;
 		}
 
+		// Properties for the Numerator and Denominator int
 		public int Numerator { get; set; }
 		public int Denominator { get; set; }
-
 		#endregion
 
-
+		// ToString() method that overrides the default ToString() method
 		public override string ToString()
 		{
 			return $"{Numerator}/{Denominator}";
 		}
 
+		// Overriding Equals() to work with objects
 		public override bool Equals(object? obj)
 		{
 			if (obj is Fraction other)
@@ -31,6 +34,12 @@ namespace Fraction
 			return false;
 		}
 
+		// Equals() method that checks if one fraction equals another
+		public bool Equals(Fraction other)
+		{
+			//return Numerator == other.Denominator && Denominator == other.Numerator;
+			return (Numerator * other.Denominator) == (Denominator * other.Numerator);
+		}
 
 		public Fraction Add(Fraction other) {
 			int newNumerator = Numerator * other.Denominator + Denominator * other.Numerator;
